7ad04a501f4c5b0f82c8f6e9c4c0197683691b9e
kai815 <h.kaizuka815@gmail.com>
2020-10-22 09:02:45 +0900

    modify error

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,4 +1,36 @@
-export default const getStaticProps = async (context) => {
+import styles from '../styles/Home.module.scss'
+
+export default function BlogId({blog}) {
+    return (
+      <main className={styles.main}>
+        <h1 className={styles.title}>{blog.title}</h1>
+        <p className={styles.publishedAt}>{blog.publishedAt}</p>
+        <p className={styles.category}>{blog.category && `${blog.category.name}`}</p>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${blog.body}`,
+          }}
+          className={styles.post}
+        />
+      </main>
+    );
+  }
+
+
+export const getStaticPaths = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  
+  const data = await fetch('https://hideo.microcms.io/api/v1/blog', key)
+    .then(res => res.json())
+    .catch(() => null);
+  
+  const paths = data.contents.map(content => `/blog/${content.id}`);
+  return {paths, fallback: false};
+};
+
+export const getStaticProps = async (context) => {
   const slug = context.params?.slug;
   const draftKey = context.previewData?.draftKey;
   const content = await fetch(
